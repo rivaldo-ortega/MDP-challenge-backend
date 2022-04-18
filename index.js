016550a1fbd22db583ec2e650e597b70e672eacc
Rivaldo <ortegaalvarezrivaldo@gmail.com>
2022-04-18 00:10:29 -0500

    URL changed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api/users', usersRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT_SERVE || 5000;
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     app.listen(port, () => {
