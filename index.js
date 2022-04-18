eabd24712ded102353bb1720bb079dddd1ac705c
Rivaldo <ortegaalvarezrivaldo@gmail.com>
2022-04-17 23:22:41 -0500

    space modification

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('React&Node Challenge');
+  res.send('React&Node Challenge ');
 });
 app.use('/api/users', usersRouter);
 
